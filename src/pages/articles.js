1005d0f8f467ab1da24eeb8e6c78a0f82542fa65
TiagoAlmeida13 <tyygo@live.com>
2023-12-08 07:43:50 -0300

    responsive article page

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -42,7 +42,7 @@ const MovingImg = ({ title, img, link }) => {
                 style={{ x: x, y: y }}
                 initial={{ opacity: 0 }}
                 whileInView={{ opacity: 1, transition: { duration: 0.2 } }}
-                ref={imgRef} src={img} alt={title} className="z-10 w-96 h-auto hidden absolute rounded-lg" />
+                ref={imgRef} src={img} alt={title} className="z-10 w-96 h-auto hidden absolute rounded-lg md:!hidden" />
         </Link>
     )
 }
@@ -55,10 +55,11 @@ const Article = ({ img, title, date, link }) => {
             viewport={{ once: true }}
             className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center
         justify-between bg-light text-dark first:mt-0 border border-solid border-dark
-        border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light
+        border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light 
+        sm:flex-col 
         ">
             <MovingImg title={title} img={img} link={link} />
-            <span className="text-primary font-semibold pl-4 dark:text-primaryDark">{date}</span>
+            <span className="text-primary font-semibold pl-4 dark:text-primaryDark sm:self-start sm:pl-0 xs:text-sm">{date}</span>
         </motion.li>
     )
 }
@@ -81,12 +82,12 @@ const FeaturedArticle = ({ img, title, time, summary, link }) => {
                     transition={{ duration: 0.2 }}
                     priority
                     sizes="(max-width:768px) 100vw,
-              (max-width: 1200px) 50vw, 
-              50vw"
+                    (max-width: 1200px) 50vw, 
+                    50vw"
                 />
             </Link>
             <Link href={link} target="_blank">
-                <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline'>{title}</h2>
+                <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline xs:text-lg'>{title}</h2>
                 <p className='text-sm mb-2'>{summary}</p>
                 <span className='text-primary font-semibold dark:text-primaryDark'>{time}</span>
             </Link>
@@ -103,13 +104,15 @@ const articles = () => {
             </Head>
             <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light'>
                 <Layout className='pt-16'>
-                    <AnimatedText text="Palavras podem mudar o mundo!" className='mb-16' />
-                    <ul className='grid grid-cols-2 gap-16'>
+                    <AnimatedText text="Palavras podem mudar o mundo!" className='mb-16 
+                    lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl
+                    '/>
+                    <ul className='grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16 '>
                         <li>
                             <FeaturedArticle
                                 title="Build A Custom Pagination Component In Reactjs From Scratch"
                                 summary=" Learn how to build a custom pagination component in ReactJS from scratch. 
-                                         Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
+                                Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
                                 time="9 min read"
                                 link="/"
                                 img={article1}
@@ -119,7 +122,7 @@ const articles = () => {
                             <FeaturedArticle
                                 title="Build A Custom Pagination Component In Reactjs From Scratch"
                                 summary=" Learn how to build a custom pagination component in ReactJS from scratch. 
-                                         Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
+                                Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
                                 time="9 min read"
                                 link="/"
                                 img={article2}
