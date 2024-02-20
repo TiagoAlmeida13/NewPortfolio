dc69eff69bdc57adbec3b1497b26312f966c71f3
TiagoAlmeida13 <tyygo@live.com>
2024-02-20 10:33:13 -0300

    Update bugs in iphone view

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -165,7 +165,7 @@ const NavBar = () => {
 
                             <button
                                 onClick={() => setMode(mode === "light" ? "dark" : "light")}
-                                className={`ml-3 flex items-center justify-center rounded-full p-1 max-w-6
+                                className={`ml-3 flex items-center justify-center rounded-full p-1 xs:max-w-6 md:max-w-6
                     ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
                     `}
                             >
