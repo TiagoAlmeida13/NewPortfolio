bbf1327ce86aee9e75fcd01196e34e3c46c23dae
Tiago Almeida <54046640+TiagoAlmeida13@users.noreply.github.com>
2024-11-18 12:00:15 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,8 +36,8 @@ export default function Home() {
               xl:!text-5xl lg:!text-center lg:!text=6xl md:!text-5xl sm:!text-3xl
               ' />
               <p className='my-4 text-base font-medium md:text-sm sm:text-xs'>
-                Como um habilidoso desenvolvedor front-end, dedico-me a transformar ideias em aplicações web inovadoras.
-                Explore meus projetos e artigos mais recentes, mostrando minha experiência em React.js e desenvolvimento web.
+                Desenvolvedor front-end, onde me dedico a transformar ideias em aplicações web inovadoras.
+                Explore meus projetos e artigos mais recentes, com experiência em React.js e desenvolvimento web.
               </p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link href="/TiagoAlmeida_2023.pdf" target={'_blank'}
