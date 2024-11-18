f356338d31178704dbdf3f62f2ada41290ee79fb
Tiago Almeida <54046640+TiagoAlmeida13@users.noreply.github.com>
2024-11-18 12:01:27 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,8 @@ const Footer = () => {
         <footer className='w-full border-t-2 border-solid border-dark
         font-medium text-lg dark:text-light dark:border-light sm:text-base'>
             <Layout className='py-8 flex items-center justify-between lg:flex-col lg:py-6'>
-                <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
                 <div className="flex items-center lg:py-2">
-                    Build with NextJS
+                    
                     by&nbsp;<Link href="https://www.linkedin.com/in/tiago-almeida-dev/" className='underline
                     underline-offset-2 
                     ' target={"_blank"}>Tiago Machado</Link>
